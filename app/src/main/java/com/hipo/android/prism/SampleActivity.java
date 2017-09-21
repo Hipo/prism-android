af20567dbc3d61ba0b3e6c06320e2133a2420732
cagla <caglaacetin@gmail.com>
2017-09-21 12:23:26 +0300

    Init project

---
+++ app/src/main/java/com/hipo/android/prism/SampleActivity.java
@@ -0,0 +1,18 @@
+package com.hipo.android.prism;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by cagla on 21/09/2017.
+ */
+
+public class SampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sample);
+    }
+}
